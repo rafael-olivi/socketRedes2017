24bf03209d2a9da395550e61e2092de968d60909
gooshtavow <gu_fs@hotmail.com>
2017-11-03 23:50:26 -0200

    versao corrigida

--- webserver.py
+++ webserver.py
@@ -1,23 +1,27 @@
 import socket
-import cgi
-import cgitb; cgitb.enable()
+import cgi, cgitb
 ip = '127.0.0.1'
 
 Daemon1 = (ip, 9001)
 Daemon2 = (ip, 9002)
 Daemon3 = (ip, 9003)
 
+cgitb.enable()
+
 s = socket.socket(
     socket.AF_INET, socket.SOCK_STREAM)
 
 form = cgi.FieldStorage()
 
-data = '' #string de dados que será enviado
+data = '' #string de dados que sera enviado
 resp = '' #resposta da requisicao feita
- 
+
+protocolo = ''
+opBinario = ''
+total = 0
+
 #----- Info Maquina 1 ----------
-if form.getvalue('maq1_ps') or form.getvalue('maq1_df')
-	or form.getvalue('maq1_finger') or form.getvalue('maq1_uptime'):
+if form.getvalue('maq1_ps') or form.getvalue('maq1_df') or form.getvalue('maq1_finger') or form.getvalue('maq1_uptime'):
 
 	if form.getvalue('maq1_ps'):
 		#Protocolo 8 bits
@@ -37,44 +41,61 @@ if form.getvalue('maq1_ps') or form.getvalue('maq1_df')
 		protocolo = '00000100'
 		options1 = form.getvalue('maq1_uptime')
 
-	if options1 = None:
-		char = 0
+	if options1 == None:
+		c = 0
 	else:
-		char = len(options1)
+		c = len(options1)
 		#conversao da string em binario
 		opBinario = ''.join(format(ord(x), 'b') for x in options1)
 
+	total = 160 + (c * 4)
 
-# ----- Informações do cabeçalho ---------
+# ----- Informacoes do cabecalho ---------
 #Versao do protocolo 4 bits = 2
 version = '0010'
+
+#IHL 4 bits 
+ihl = '1111'
+
 #Type of service 8 bits = 0
 tos = '00000000'
+
+#Total Length 16 bits
+	#bl = total.bit_length()
+total_length = 8 * '0' + bin(total)[2:]
+
 #Identification 16 bits 
 identification = '1000000000000000'
+
 #Flags 3 bits - 000 se requisicao
 flags = '000'
+
 #Fragment Offset 13 bits = 0
 fOffset = '0000000000000'
+
 #Time to live 8 bits = 4
 ttl = '00000100'
+
+#Header_Checksum 16 bits - Verificacao
+header_checksum = '0000000000000000'
+
 #Source adress 32 bits - 192.168.56.101
 s_addr =  '11000000' #192 
 s_addr += '10101000' #168 
 s_addr += '00111000' #56
 s_addr += '01100101' #101
+
 #Destnation adress 32 bits - 192.168.56.1
 d_addr =  '11000000' #192
 d_addr += '10101000' #168
 d_addr += '00111000' #56
 d_addr += '00000001'
 
-#Header_Checksum 16 bits - Verificacao
-header_checksum = '0000000000000000'
+#Padding 
+padding = '00000000'
 
-#Junção das informações 
-data += version + IHL + tos + total_length + identification + flags + fOffset
-+ ttl + protocolo + header_checksum + s_addr + d_addr + opBinario
+#Juncao das informacoes 
+data += version + ihl + tos + total_length + identification + flags + fOffset + ttl + protocolo + header_checksum + s_addr + d_addr + opBinario + padding
 
 #Cria a conexao e envia os dados
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -93,7 +114,7 @@ s.close()
 
 
 
-#--------- Mostrar informações --------
+#--------- Mostrar informacoes --------
 
 print ("Content-Type: text/html\n\n")
 print (resp)
